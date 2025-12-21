0a19b286abbe0bb6d6b1c908e801fd9dc2b47e02
Sofia Besenski <sofiabesenski4@gmail.com>
2025-12-20 16:39:47 -0800

    Set currency to CAD

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -7,7 +7,7 @@ Spree.load_defaults '4.6.2'
 Spree.config do |config|
   # Core:
   # Default currency for new sites
-  config.currency = "USD"
+  config.currency = "CAD"
 
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
